e0b0c5acdf5e03b9ff96324715646bf3c08dfdec
Rishabh Jain <70846152+Rishabhjain2002@users.noreply.github.com>
2021-07-06 21:43:15 +0530

    tsk-9

---
+++ index.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+// fs.mkdirSync("Rishabh");
+// fs.writeFileSync("task.txt","Myself Rishabh , Member of ACM society")
+fs.appendFileSync("task.txt",". I am going to be a great coder for sure. ")
+// const data = fs.readFileSync("task.txt","utf-8");
+// console.log(data);
+// fs.renameSync("task.txt", "task9.txt");
+// fs.unlinkSync("task9.txt");
